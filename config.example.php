bb3de1c62cc05218e38957f1696119d86cb448fa
Suyash Taral <suyashtaral83@gmail.com>
2026-07-14 22:54:29 +0530

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Database Configuration
+|--------------------------------------------------------------------------
+|
+| Copy this file as config.php
+| Fill your own credentials.
+|
+*/
+
+$host="localhost";
+
+$user="YOUR_DATABASE_USERNAME";
+
+$password="YOUR_DATABASE_PASSWORD";
+
+$database="YOUR_DATABASE_NAME";
